288e143f0f65d34fd70a72cba5557c25de6bb5b0
b0b1kk <9bo.obi@gmail.com>
2019-09-04 22:55:52 +0300

    Initial Commit

---
+++ 8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8ball
+//
+//  Created by Bogdan Mishura on 9/4/19.
+//  Copyright © 2019 Bogdan Mishura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
